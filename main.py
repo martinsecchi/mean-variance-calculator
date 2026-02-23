097f33bbc866c3bad27d9dc5fe70225c46fc21de
Martin Miguel Custidiano Secchi <martinsecchi@gmail.com>
2026-02-23 16:35:39 -0300

    Add main.py entry point

---
+++ main.py
@@ -0,0 +1,8 @@
+from mean_var_std import calculate
+from unittest import main
+
+# Prueba rapida
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+# Ejecutar tests automaticos
+main(module='test_module', exit=False)
